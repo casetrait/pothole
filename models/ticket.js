70ff64fe3e17d723b25d9327314b11a46eaa2eef
Upenatom <randolphcrasto1@gmail.com>
2022-10-08 11:48:57 -0500

    prelim models

---
+++ models/ticket.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema(
+  {
+    url: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ticketSchema = new Schema(
+  {
+    reporter: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    title: String,
+    category: {
+      type: String,
+      enum: [
+        "Pothole",
+        "Drainage",
+        "Pedestrian",
+        "Traffic Light",
+        "Signage",
+        "Speed Limit",
+        "Snow",
+        "Ice",
+      ],
+    },
+    description: String,
+    lat: Number,
+    long: Number,
+    confirmationVote: [mongoose.Schema.Types.ObjectId],
+    resolvedVote: [mongoose.Schema.Types.ObjectId],
+    images: [imageSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Ticket", ticketSchema);
