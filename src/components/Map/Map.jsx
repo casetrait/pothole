fb187ec65575ee46ede6c7ed95655eb5dfafeda7
Upenatom <randolphcrasto1@gmail.com>
2022-10-12 07:51:16 -0500

    cluster working

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,12 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import MarkerClusterGroup from '@changey/react-leaflet-markercluster';
 import Getclick from './Getclick'
 import './Map.css'
 import {iconSelect} from '../../utilities/iconSelector'
 
 export default  function Map({setPosition,statePosition,ticketItems,formCheck}) {
 
+  
 
     return (
         
@@ -13,20 +15,22 @@ export default  function Map({setPosition,statePosition,ticketItems,formCheck}) 
         zoom={13}
         scrollWheelZoom={true}
         >
+          
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-       
+       <MarkerClusterGroup>
         {ticketItems.map(ticket=>
-        <Marker key = {ticket._id} position={[ticket.lat, ticket.long]} icon = {iconSelect(ticket.category)}>
+        <Marker key = {ticket._id} position={[ticket.lat, ticket.long]} icon = {iconSelect(ticket.category)
+}>
           <Popup>
-            {ticket.category} <br/>
+            <b>{ticket.category}</b> <br/>
             {ticket.title}
           </Popup>
           </Marker>)}
-        
-        {/* Make Sure get click and selection marker only works on form page*/}
+          </MarkerClusterGroup>
+        {/* This is to make Sure get click and selection marker only works on form page*/}
         {formCheck? <Getclick setPosition={setPosition} statePosition={statePosition}/>: null}
                
         {formCheck? <Marker position={statePosition} ></Marker>: null}
