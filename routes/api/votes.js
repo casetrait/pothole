e56a88a82674eff92ae075ac64fde8c6d057a72e
Kyle Strate <kstrate@MacBook-Pro.local>
2022-10-13 11:07:49 -0600

    auth and styling

--- routes/api/votes.js
+++ routes/api/votes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const votesCtrl = require("../../controllers/api/votes");
 
+
+router.use(require("../../config/auth"));
 //edit users votes
 
 //add conVote
 router.put("/addCon/:id", votesCtrl.addConVote)
 //remove conVote
 router.put("/removeCon/:id", votesCtrl.removeConVote)
-
 //add resVote
 router.put("/addRes/:id", votesCtrl.addResVote)
 //remove resVote
