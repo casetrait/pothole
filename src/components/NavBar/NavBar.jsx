4b59b1e74a82c6a6b79264bd87a40c15ce67b5c0
Upenatom <randolphcrasto1@gmail.com>
2022-10-13 13:37:15 -0500

    logos added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,10 @@
 import Logo from "../Logo/Logo";
 import { Link } from "react-router-dom";
 import './NavBar.css'
-
+import all from '../../static/images/all.png'
+import New from '../../static/images/new.png'
+import logout from '../../static/images/logout.png'
+import login from '../../static/images/login.png'
 
 
 export default function NavBar({handleLogout}) {
@@ -12,14 +15,14 @@ export default function NavBar({handleLogout}) {
         jwt ? (
             <nav className="nav-grid">
                 <Link to="/home" className="menu-img"><Logo /></Link>
-                <Link to="/tickets" className="menu-img" id="all"><img src={process.env.PUBLIC_URL + '/images/all.png'} width='200px' alt ='All Tickets'/></Link>
-                <Link to="/new" className="menu-img"><img src={process.env.PUBLIC_URL + '/images/new.png'} width='200px' alt ='New Ticket'/></Link>
-                <Link className="menu-img" onClick={()=>handleLogout()}><img src={process.env.PUBLIC_URL + '/images/logout.png'} width='200px' alt ='Logout'/></Link>
+                <Link to="/tickets" className="menu-img" id="all"><img src={all} width='200px' alt ='All Tickets'/></Link>
+                <Link to="/new" className="menu-img"><img src={New} width='200px' alt ='New Ticket'/></Link>
+                <Link className="menu-img" onClick={()=>handleLogout()}><img src={logout} width='200px' alt ='Logout'/></Link>
             </nav>
         ) : (
             <nav className="nav-grid">
                 <Link to="/home" className="menu-img"><Logo /></Link>
-                <Link to="/login" className="menu-img"><img src={process.env.PUBLIC_URL +'/images/login.png'} width='200px' alt ='Login'/></Link>
+                <Link to="/login" className="menu-img"><img src={login} width='200px' alt ='Login'/></Link>
             </nav>
         )
     );
