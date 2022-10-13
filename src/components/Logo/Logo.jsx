97d343e82842d44db7581f7692f1e83628a76fe4
Upenatom <randolphcrasto1@gmail.com>
2022-10-13 13:19:41 -0500

    test images

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo(props) {
     return (
         <div className="">
-            <img src='/images/logo.png' width='200px' alt ='Logo'/>
+            <img src= {process.env.PUBLIC_URL+'/images/logo.png'} width='200px' alt ='Logo'/>
         </div>
     );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import './NavBar.css'
 
 
+
 export default function NavBar({handleLogout}) {
     
     let jwt = localStorage.getItem('token')
@@ -11,14 +12,14 @@ export default function NavBar({handleLogout}) {
         jwt ? (
             <nav className="nav-grid">
                 <Link to="/home" className="menu-img"><Logo /></Link>
-                <Link to="/tickets" className="menu-img" id="all"><img src='/images/all.png' width='200px' alt ='All Tickets'/></Link>
-                <Link to="/new" className="menu-img"><img src='/images/new.png' width='200px' alt ='New Ticket'/></Link>
-                <Link className="menu-img" onClick={()=>handleLogout()}><img src='/images/logout.png' width='200px' alt ='Logout'/></Link>
+                <Link to="/tickets" className="menu-img" id="all"><img src={process.env.PUBLIC_URL + '/images/all.png'} width='200px' alt ='All Tickets'/></Link>
+                <Link to="/new" className="menu-img"><img src={process.env.PUBLIC_URL + '/images/new.png'} width='200px' alt ='New Ticket'/></Link>
+                <Link className="menu-img" onClick={()=>handleLogout()}><img src='/static/images/logout.png' width='200px' alt ='Logout'/></Link>
             </nav>
         ) : (
             <nav className="nav-grid">
                 <Link to="/home" className="menu-img"><Logo /></Link>
-                <Link to="/login" className="menu-img"><img src='/images/login.png' width='200px' alt ='Login'/></Link>
+                <Link to="/login" className="menu-img"><img src={process.env.PUBLIC_URL +'/images/login.png'} width='200px' alt ='Login'/></Link>
             </nav>
         )
     );
