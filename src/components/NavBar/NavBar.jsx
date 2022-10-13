edfffc521d9747e8901a74d13cdc8103fa4ba47b
Upenatom <randolphcrasto1@gmail.com>
2022-10-13 13:22:21 -0500

    push heroku

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ export default function NavBar({handleLogout}) {
                 <Link to="/home" className="menu-img"><Logo /></Link>
                 <Link to="/tickets" className="menu-img" id="all"><img src={process.env.PUBLIC_URL + '/images/all.png'} width='200px' alt ='All Tickets'/></Link>
                 <Link to="/new" className="menu-img"><img src={process.env.PUBLIC_URL + '/images/new.png'} width='200px' alt ='New Ticket'/></Link>
-                <Link className="menu-img" onClick={()=>handleLogout()}><img src='/static/images/logout.png' width='200px' alt ='Logout'/></Link>
+                <Link className="menu-img" onClick={()=>handleLogout()}><img src={process.env.PUBLIC_URL + '/images/logout.png'} width='200px' alt ='Logout'/></Link>
             </nav>
         ) : (
             <nav className="nav-grid">
