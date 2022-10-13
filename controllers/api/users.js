c2d3d23ea1875b01a6f02e82c2debf70d17fbd06
Upenatom <randolphcrasto1@gmail.com>
2022-10-13 12:59:06 -0500

    heroku fix

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,35 +1,38 @@
-const User = require('../../models/User')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 module.exports = {
-  create, 
-  login
-}
+  create,
+  login,
+};
 
 async function create(req, res) {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, parseInt(process.env.SALT_ROUNDS))
+    const hashedPassword = await bcrypt.hash(
+      req.body.password,
+      parseInt(process.env.SALT_ROUNDS)
+    );
     const user = await User.create({
-      email: req.body.email, 
+      email: req.body.email,
       password: hashedPassword,
-    })
-    console.log(user)
-    const token = jwt.sign({user}, process.env.SECRET, {expiresIn: '24h'})
-    res.status(200).json(token)
+    });
+    console.log(user);
+    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
+    res.status(200).json(token);
   } catch (err) {
-    res.status(400).json(err)
+    res.status(400).json(err);
   }
 }
 
 async function login(req, res) {
   try {
-    const user = await User.findOne({email: req.body.email})
-    if (!(await bcrypt.compare(req.body.password, user.password))) throw new Error("Password doesn't match e-mail")
-    const token = jwt.sign({user}, process.env.SECRET, { expiresIn: '24h' })
-    res.status(200).json(token)
+    const user = await User.findOne({ email: req.body.email });
+    if (!(await bcrypt.compare(req.body.password, user.password)))
+      throw new Error("Password doesn't match e-mail");
+    const token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
+    res.status(200).json(token);
   } catch (err) {
-    res.status(400).json(err)
+    res.status(400).json(err);
   }
 }
-
